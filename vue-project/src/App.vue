17f293583c26776cef433aa7e10ba979127f6dda
disservin <disservin.social@gmail.com>
2024-04-24 09:25:05 +0200

    Add files via upload

---
+++ vue-project/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <router-view v-slot="{ Component, route }">
+    <transition name="fade" mode="out-in">
+      <keep-alive>
+        <component :is="Component" :key="route.fullPath" />
+      </keep-alive>
+    </transition>
+  </router-view>
+</template>
+
+<style scoped></style>
